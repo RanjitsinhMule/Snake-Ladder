40681bf6b4186c5d858b47acdb6ff1fd786bf1c4
RanjitsinhMule <muleranjitsinh7323@gmail.com>
2022-09-01 22:19:30 +0530

    single player position 0

---
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.snakeandladder;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+	
+	//Initially we start the game with one player
+			int player=1;
+
+			// The player in position 0
+			int position=0;
+
+			System.out.println("Player" +player+ " in the position = "+position);
+		
+	}
+  }
+
+
+
